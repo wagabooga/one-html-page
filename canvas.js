838e1ce1b040bdf4c33c4fd29b1de0f7c7432eb5
wagabooga <64753034+wagabooga@users.noreply.github.com>
2021-12-12 00:19:11 -0800

    drawing squares and lines on canvas

--- canvas.js
+++ canvas.js
@@ -8,6 +8,21 @@ const c = canvas.getContext('2d');
 
 // --------------------- NOTES ---------------------
 // c.fillRect(x, y, width, height) creates a rectangle at x,y cooridnates and the size
-// c.moveTo(x, y) draws a line from x to y
+// c.fillStyle = "rgba( 255, 0, 0 ,0.1)" fills rectangles (0.1 is transparency)
+// c.moveTo(x, y) imaginary paintbrush a line from x to y (needs stroke method to actually draw)
+// to add color add c.strokeStyle = "rgba() || blue || #f3f3f3"
 // --------------------- ----- ---------------------
+// rect
+c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+c.fillRect(300, 450, 100, 100);
+c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+c.fillRect(700, 307, 100, 100);
+// line
+c.beginPath()
+c.moveTo(50, 300);
+c.lineTo(400, 300);
+c.lineTo(600, 500);
+c.strokeStyle = "red";
+c.stroke()
