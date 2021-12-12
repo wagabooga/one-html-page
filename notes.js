9eda54708a6697e4d5fb0647b43f3732603354c8
wagabooga <64753034+wagabooga@users.noreply.github.com>
2021-12-12 02:13:37 -0800

    added canvas refresh for resize and logging x/y coords on click

--- notes.js
+++ notes.js
@@ -123,3 +123,15 @@ function spinnerCursorAnimate() {
 
 }
 spinnerCursorAnimate()
+
+
+
+document.onmousemove = function(event) {
+    pointerX = event.pageX;
+    pointerY = event.pageY;
+}
+setInterval(pointerCheck, 1000);
+
+function pointerCheck() {
+    //console.log('Cursor at: ' + pointerX + ', ' + pointerY);
+}
